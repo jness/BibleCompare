3c3c878ec58c24337b73a1d5dca1d056c45398d7
Jeffrey Ness <jness@flip-edesign.com>
2012-12-27 13:15:45 -0600

    first

---
+++ main.py
@@ -0,0 +1,18 @@
+from Bible import Bible
+from simplediff import html_diff
+
+# open our two bibles 
+kjv = Bible('bibles/kjv.xml')
+nkjv = Bible('bibles/nkjv.xml')
+
+# go in order through all books, chapters, verses
+for book in kjv.bible:
+    for chapter in kjv.bible[book]:
+        for verse in kjv.bible[book][chapter]:
+            kjv_body = kjv.bible[book][chapter][verse]
+            nkjv_body = nkjv.bible[book][chapter][verse]
+            
+            # Only print is something is differnt
+            if kjv_body != nkjv_body:
+                print '<p><b>[%s %s:%s]</b> %s</p><br>' % \
+                    (book, chapter, verse, html_diff(kjv_body, nkjv_body))
